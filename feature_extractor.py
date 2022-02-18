ae7c97922544fa727e9481c2912223056c4fc237
Surbhi <srv2410@gmail.com>
2022-02-18 19:25:43 +0530

    Initial commit

---
+++ feature_extractor.py
@@ -0,0 +1,12 @@
+import pickle
+import os
+
+actors = os.listdir('data')
+
+filenames = []
+for actor in actors:
+    for file in os.listdir(os.path.join('data',actor)):
+        filenames.append(os.path.join('data',actor,file))
+
+pickle.dump(filenames,open('filenames.pkl','wb'))
+
